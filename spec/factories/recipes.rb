8fdf39952adb8fc292b67f7a1235496de2385d8a
João Torquato <joao.otl@gmail.com>
2016-02-08 20:38:32 -0200

    rubocop

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -4,7 +4,9 @@ FactoryGirl.define do
     servings 4
     preparation_time 90
     difficulty "Médio"
-    ingredients "400g de carne moída, 1 colher de chpa de garam masala, azeite de oliva, 3 pimentôes vermelhos"
-    steps "Coloque 1 cancea de trigo bulgur, 2 canecas de água fervente, o limão em conversa, tampe e mexa"
+    ingredients "400g de carne moída, 1 colher de chpa de garam masala,
+                 azeite de oliva, 3 pimentôes vermelhos"
+    steps "Coloque 1 cancea de trigo bulgur,
+           2 canecas de água fervente, o limão em conversa, tampe e mexa"
   end
 end
